0f66a0fabdd32f50c5c7367ae62dd0a038ab1acf
Giacomo Rossetto <jackyman_cs4@live.it>
2023-08-27 21:20:09 +0200

    Update

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ parallel python3 main.py ::: {1..100}
 
 parallel --retries 3 python3 -m wikiart_framer ::: {1..100}
 
-python3 -m wikiart_framer
+python3 -m wikiart_framer --help
--- wikiart_framer/__main__.py
+++ wikiart_framer/__main__.py
@@ -1,18 +1,12 @@
 import sys
-
-if __package__ is None and not hasattr(sys, "frozen"):
-    import os.path
-
-    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.insert(0, os.path.realpath(path))
-
+import typer
 import wikiart_framer
+import glob
 
-wikiart_framer.box.Box()
+app = typer.Typer()
 
-if __name__ == "__main__":
-    # sys.exit(gallery_dl.main())
-    print("Ottengo nuova arte")
+@app.command()
+def download_one_random():
     url = wikiart_framer.ottieni_arte()
     file = wikiart_framer.download_image(url)
 
@@ -27,30 +21,52 @@ if __name__ == "__main__":
 
     if FILE_PATH != "":
         wikiart_framer.process_image(FILE_PATH, FILE_NAME, screen_box)
-        sys.exit(0)
     else:
         print("Nessun persorso disponibile")
 
-    sys.exit(1)
+@app.command()
+def download_many(number: int):
+    # url = "https://www.wikiart.org/en/ivan-bilibin/sketch-for-the-opera-the-golden-cockerel-by-nikolai-rimsky-korsakov-1909-1"
+    for lp in range(number):
+        download_one_random()
 
 
-# print("Ottengo nuova arte")
+@app.command()
+def download_one(url: str):
+    # url = "https://www.wikiart.org/en/ivan-bilibin/sketch-for-the-opera-the-golden-cockerel-by-nikolai-rimsky-korsakov-1909-1"
+    file = wikiart_framer.download_image(url)
 
-# for lp in range(100):
+@app.command()
+def process_all():
 
-# url = ottieniArte()
-# url = "https://www.wikiart.org/en/ivan-bilibin/sketch-for-the-opera-the-golden-cockerel-by-nikolai-rimsky-korsakov-1909-1"
-# file = downloadImage(url)
+    screen_box = wikiart_framer.box.Box()
+    screen_box.set_width(480)
+    screen_box.set_height(234)
 
-# processDownloaded()
+    wikiart_framer.process_downloaded(screen_box)
 
+@app.command()
+def process_one(file_name: str):
 # filepath = "gallery-dl/wikiart/Theophile Steinlen/577287e3edc2cb388008a5ef_Ces Dames Lafforest  Original drawing.jpg"
-# filename = "test.jpg"
 
-# filename = str(file.filename)
-# filepath = str(file.path)
+    screen_box = wikiart_framer.box.Box()
+    screen_box.set_width(480)
+    screen_box.set_height(234)
 
-# if filepath != '':
-#     processImage(filepath, filename)
-# else:
-#     print('Nessun persorso disponibile')
+    dir_path = r"./gallery-dl/wikiart/*/*.*"
+    res = glob.glob(dir_path)
+
+    done = False
+
+    for filepath in res:
+        filename = filepath.split("/")[-1]
+
+        if(filename == file_name):
+            done = True
+            wikiart_framer.process_image(filepath, file_name, screen_box)
+
+    if not done:
+        print('Nessun persorso disponibile')
+
+if __name__ == "__main__":
+    app()
